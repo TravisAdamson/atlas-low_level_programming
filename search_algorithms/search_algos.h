e6066c883ced5cf149585a38abddda779f870742
Travis <travis.adamson@atlasschool.com>
2025-01-09 11:52:03 -0600

    Fixing header proto

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -6,9 +6,8 @@
 #define POSITION(l, h, a, v) \
 	(l + (((double)(h - l) / (a[h] - a[l]))) * (v - a[l]))
 
-void print_array(int *array, size_t low, size_t high);
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t min, size_t max, size_t size, int value);
+void print_search(int *array, size_t min, size_t max);
 
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
